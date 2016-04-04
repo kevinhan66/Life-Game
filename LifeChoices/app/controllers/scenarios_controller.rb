4419a40d1ad5271803a9d53aac8b95ea57dcec5d
pumak254 <cesar_chavira@att.net>
2016-04-04 14:13:49 +0000

    Routine Commit

--- LifeChoices/app/controllers/scenarios_controller.rb
+++ LifeChoices/app/controllers/scenarios_controller.rb
@@ -1,2 +1,21 @@
 class ScenariosController < ApplicationController
+
+    def index 
+       # @scenarios = Scenario.all
+    end
+    
+    def show
+        id = params[:id]
+        @scenario = Scenario.find(id)
+    end
+
 end
+
+
+=begin
+def show
+    id = params[:id] # retrieve movie ID from URI route
+    @movie = Movie.find(id) # look up movie by unique ID
+    # will render app/views/movies/show.<extension> by default
+  end
+=end
