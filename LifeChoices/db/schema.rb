93cafe3f461351ea1c805d6b720208fc20593704
pumak254 <cesar_chavira@att.net>
2016-04-24 18:15:59 +0000

    Add login/signup functionality

--- LifeChoices/db/schema.rb
+++ LifeChoices/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160404204439) do
+ActiveRecord::Schema.define(version: 20160424180244) do
 
   create_table "choices", force: :cascade do |t|
     t.string   "option"
@@ -33,6 +33,13 @@ ActiveRecord::Schema.define(version: 20160404204439) do
     t.integer  "choice_3"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
